9f2961624e8c1738b1e59e4d7f8c197e5105ce1f
laporkka <alexsolodkiy2008@gmail.com>
2026-09-12 00:38:26 +0300

    exeption logic

--- reachout/src/api/dependencies.py
+++ reachout/src/api/dependencies.py
@@ -1,5 +1,5 @@
 from jose import jwt
-from fastapi import status, HTTPException, Depends
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,6 +11,7 @@ from reachout.src.core.database import get_db
 from reachout.src.models.user import Manager
 from reachout.src.core.config import settings
 from reachout.src.services.redis import get_redis
+from reachout.src.core.exeptions import InvalidCredentialsError
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/managers/login")
@@ -21,12 +22,6 @@ async def get_current_user(
         db: AsyncSession = Depends(get_db),
         redis_cli: Redis = Depends(get_redis)
 ) -> Manager:
-    creditionals_exeption = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
-
     try:
         payload = jwt.decode(token=token, key=settings.SECRET_KEY, algorithms=settings.ALGORITHM)
 
@@ -34,21 +29,21 @@ async def get_current_user(
         email: EmailStr | None = payload.get("sub")
 
         if not email or not jti:
-            raise creditionals_exeption
+            raise InvalidCredentialsError()
             
     except JWTError:
-        raise creditionals_exeption
+        raise InvalidCredentialsError()
 
     is_blacklisted = await redis_cli.get(f"blacklist:{jti}")
     
     if is_blacklisted is not None:
-        raise creditionals_exeption
+        raise InvalidCredentialsError()
 
     query = select(Manager).filter(Manager.email == email)
     result = await db.execute(query)
     manager = result.scalar_one_or_none()
 
     if not manager:
-        raise creditionals_exeption
+        raise InvalidCredentialsError()
 
     return manager
---
+++ reachout/src/core/exeptions.py
@@ -0,0 +1,34 @@
+from fastapi import status, HTTPException
+
+
+class UserExistError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User already exist"
+        )
+    
+
+class IvalidLoginPasswordError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect login or password"
+        )
+
+
+class InvalidTokenError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token state during logout request"
+        )
+
+
+class InvalidCredentialsError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
